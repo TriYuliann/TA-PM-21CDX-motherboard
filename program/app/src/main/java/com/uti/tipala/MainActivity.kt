5efc820987d8382e7d004b9e31ee6c0bc4820ac5
TriYuliann <mayariana1206@gmail.com>
2024-06-08 21:38:29 +0700

    penambahan metode binding

--- program/app/src/main/java/com/uti/tipala/MainActivity.kt
+++ program/app/src/main/java/com/uti/tipala/MainActivity.kt
@@ -2,15 +2,26 @@ package com.uti.tipala
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.activity.OnBackPressedCallback
+import com.uti.tipala.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // deklarasi variabel "binding"
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        // buat variabel "menu"
+        var menu = 1
+        setContentView(binding.root)
+
         // buka "fragment1"
         supportFragmentManager.beginTransaction().replace(R.id.frm_content, Menu1Fragment())
             .commit()
+
+
     }
 
 
