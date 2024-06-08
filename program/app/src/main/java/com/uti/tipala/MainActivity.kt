a8f400e05114d9300023c6f4dd4a4741ab28b8b9
TriYuliann <mayariana1206@gmail.com>
2024-06-08 21:02:04 +0700

    penambahan code pada mainactivity.kt untuk membuka fragment1

--- program/app/src/main/java/com/uti/tipala/MainActivity.kt
+++ program/app/src/main/java/com/uti/tipala/MainActivity.kt
@@ -7,6 +7,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // buka "fragment1"
+        supportFragmentManager.beginTransaction().replace(R.id.frm_content, Menu1Fragment())
+            .commit()
     }
+
 
 }
