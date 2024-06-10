29a35c49c3b0373af0307e2c657745e06637efb3
TriYuliann <mayariana1206@gmail.com>
2024-06-11 00:00:16 +0700

    membuat activity menu_login dan mengatur agar pertama kali ditampilkan di layar ketika aplikasi dijalankan

---
+++ program/app/src/main/java/com/uti/tipala/menu_login.kt
@@ -0,0 +1,15 @@
+package com.uti.tipala
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.activity.OnBackPressedCallback
+import com.uti.tipala.databinding.ActivityMainBinding
+
+class menu_login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.menu_login)
+        
+    }
+}
