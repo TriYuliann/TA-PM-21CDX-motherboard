d9fc81fcfb2d1c096b4c27ec4d413b81fccc021d
TriYuliann <mayariana1206@gmail.com>
2024-06-11 00:19:10 +0700

    membuat deklarasi varibel lalu membuat event pada btnLogin agar ketika di klik button login menuju menu utama

--- program/app/src/main/java/com/uti/tipala/menu_login.kt
+++ program/app/src/main/java/com/uti/tipala/menu_login.kt
@@ -1,8 +1,10 @@
 package com.uti.tipala
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import androidx.activity.OnBackPressedCallback
 import com.uti.tipala.databinding.ActivityMainBinding
 
@@ -10,6 +12,17 @@ class menu_login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.menu_login)
-        
+
+        // deklarasi variabel komponen
+        val btnLogin: ImageView = findViewById(R.id.btnLogin)
+
+        // 2. event untuk "btnlogin"
+        btnLogin.setOnClickListener {
+            // buka "main_activity/menu utama apk"
+            //Intent(Sumber, Tujuan)
+            val intent = Intent(this,
+                menu_login::class.java)
+            startActivity(intent)
+        }
     }
 }
