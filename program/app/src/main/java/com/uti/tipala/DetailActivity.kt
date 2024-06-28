4520b92c9603816bb20c22de6fbc2ac3021da763
TriYuliann <mayariana1206@gmail.com>
2024-06-28 22:45:54 +0700

    memperbaiki bug forceclose lalu mengubah detailActivity

--- program/app/src/main/java/com/uti/tipala/DetailActivity.kt
+++ program/app/src/main/java/com/uti/tipala/DetailActivity.kt
@@ -1,55 +1,25 @@
 package com.uti.tipala
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.ImageView
+import android.widget.TextView
 
-/**
- * A simple [Fragment] subclass.
- * Use the [DetailActivity.newInstance] factory method to
- * create an instance of this fragment.
- */
-class DetailActivity : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class DetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+        setContentView(R.layout.fragment_detail_activity)
+
+        val getData = intent.getParcelableExtra<DataClass>("android")
+        if (getData != null) {
+            val detailTitle: TextView = findViewById(R.id.detailTitle)
+            val detailDesc: TextView = findViewById(R.id.detailDesc)
+            val detailImage: ImageView = findViewById(R.id.detailImage)
+
+            detailTitle.text = getData.dataTitle
+            detailDesc.text = getData.dataDesc
+            detailImage.setImageResource(getData.dataDetailImage)
         }
-    }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_activity, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DetailActivity.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic fun newInstance(param1: String, param2: String) =
-                DetailActivity().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
     }
 }
